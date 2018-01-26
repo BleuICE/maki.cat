29ac7d25b00ebc8b4a1ff3a42c8809c2a98c2531
Maki <mxmcube@gmail.com>
2018-01-26 11:48:09 +0000

    nice

--- routes/systems/screenshot.js
+++ routes/systems/screenshot.js
@@ -5,14 +5,15 @@ global.app.get("/screenshot/:service", function(req, res) {
 	
 	switch (req.params.service) {
 		case "discord-celeste":
-			
-			new Screenshot('http://ghub.io/')
-				.width(800).height(600)
+
+			new Screenshot("https://discordapp.com/widget?id=406379938468331522&theme=dark")
+				.width(285).height(400)
 				.capture().then(function(img) {
+					res.set("Content-Type", "image/png");
 					res.send(img);
-			});
+				});
 			break;
-			
+
 		default:
 
 			res.send("Service not found.")
