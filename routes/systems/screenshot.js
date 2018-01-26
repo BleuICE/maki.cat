1de657c515dd5d4358fb77c65c2604c90aa50944
Maki <mxmcube@gmail.com>
2018-01-26 11:31:08 +0000

    nice

--- routes/systems/screenshot.js
+++ routes/systems/screenshot.js
@@ -4,12 +4,19 @@ var Screenshot = require("url-to-screenshot");
 global.app.get("/screenshot/:service", function(req, res) {
 	
 	switch (req.params.service) {
-		case "discord":
-			res.send("nice");
+		case "discord-celeste":
+			
+			new Screenshot('http://ghub.io/')
+				.width(800).height(600)
+				.capture().then(function(img) {
+					res.send(img);
+			});
 			break;
+			
 		default:
+
 			res.send("Service not found.")
-			break;		
+			break;
 	}
 
 });
