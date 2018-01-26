6ed85c4ed488a5604aecff6336573ec7d7550b69
Maki <mxmcube@gmail.com>
2018-01-26 11:25:27 +0000

    nice

--- routes/systems/screenshot.js
+++ routes/systems/screenshot.js
@@ -1,7 +1,15 @@
 var fs = require("fs");
+var Screenshot = require("url-to-screenshot");
 
-global.app.get("/screenshot", function(req, res) {
+global.app.get("/screenshot/:service", function(req, res) {
 	
-
+	switch (req.params.service) {
+		case "discord":
+			res.send("nice");
+			break;
+		default:
+			res.send("Service not found.")
+			break;		
+	}
 
 });
